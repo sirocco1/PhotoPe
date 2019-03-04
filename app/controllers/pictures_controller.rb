3cedb3463c4429c94e71097ad06a82f57620664f
sirocco1 <shino2163@gmail.com>
2019-03-04 12:30:12 +0900

    Add: 画像投稿機能/リサイズ機能を追加
    ・Gem CarrierWaveとMini-Magickをインストール
    ・pictures new画面に画像投稿機能を追加
    ・画像リサイズ機能を追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,15 +4,25 @@ class PicturesController < ApplicationController
   end
 
   def new
+    @picture = Picture.new
+    @pictures = Picture.all #仮作成 / 投稿機能確認用
   end
 
   def create
+    @picture = current_user.pictures.new(picture_params)
+    @picture.save
+    redirect_to pictures_path
   end
 
   def show
   end
 
   def destroy
+  end
+
+private
+  def picture_params
+    params.require(:picture).permit(:animal_genre, :image, :video, :description)
   end
 
 end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,3 +1,13 @@
 class Picture < ApplicationRecord
 	belongs_to :user
+	mount_uploader :image, ImageUploader
+	validate :image_size
+
+	private
+	# 独自のバリデーションを定義 / アップロードされた画像サイズを制限
+	def image_size
+		if image.size > 5.megabytes
+			errors.add(:image, "should be less than 5MB")
+		end
+	end
 end
