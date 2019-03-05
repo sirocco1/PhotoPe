7073c9cb079b9416938128ce9efc812cc8c3169e
sirocco1 <shino2163@gmail.com>
2019-03-05 18:10:45 +0900

    Add: いいね!機能を搭載（レイアウト未）
    ・仮ページでお気に入り機能を追加（動作確認）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   	@pictures = @user.pictures
+    @favorite_pictures = @user.favorite_pictures
   end
 
   def edit
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :pictures
   has_many :favorites
+  has_many :favorite_pictures, through: :favorites, source: :picture
 end
