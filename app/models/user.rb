bf70e53d4a9b39c8bccb504a2b9e822a7ff18550
sirocco1 <shino2163@gmail.com>
2019-03-05 20:17:34 +0900

    Add: フォロー/フォロワー機能のアソシエーション
    ・UserモデルとRelationshipモデルでアソシエーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   has_many :pictures
   has_many :favorites
   has_many :favorite_pictures, through: :favorites, source: :picture
+
+  #フォロー/フォロワーの記述
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  has_many :followings, through: :active_relationships, source: :follower
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  has_many :followers, through: :passive_relationships, source: :following
+  def followed_by?(user)
+  	passive_relationships.find_by(following_id: user.id).present?
+  end
 end
