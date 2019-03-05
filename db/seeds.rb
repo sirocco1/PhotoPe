31fa6cf723bf5c24fcd333434e58310e9d07d9ed
sirocco1 <shino2163@gmail.com>
2019-03-05 16:45:10 +0900

    Add: ユーザ一覧画面と詳細画面追加（仮）
    ・ユーザ関連の実装フィードバックを早くするため一覧画面を仮作成
    ・user登録のseedファイルを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# coding: utf-8
+User.create(email: 'admin@example.com',   password: 'password',name: 'aaaa')
+User.create(email: 'manager@example.com', password: 'password',name: 'iiii')
+User.create(email: 'user@example.com',    password: 'password',name: 'uuuu')
+User.create(email: 'admdin@example.com',   password: 'password',name: 'eeee')
+User.create(email: 'manlager@example.com', password: 'password',name: 'ooooo')
+User.create(email: 'usler@example.com',    password: 'password',name: 'kkkk')
+User.create(email: 'admiln@example.com',   password: 'password',name: 'aakkkaa')
+User.create(email: 'manlager@example.com', password: 'password',name: 'iiikkkki')
+User.create(email: 'usekkr@example.com',    password: 'password',name: 'uuukkku')
+User.create(email: 'admikn@example.com',   password: 'password',name: 'akkkaaa')
+User.create(email: 'mankager@example.com', password: 'password',name: 'iikkkii')
+User.create(email: 'ukser@example.com',    password: 'password',name: 'uukkkuu')
