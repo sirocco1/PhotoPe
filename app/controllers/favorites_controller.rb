836e9f7b377dbc62a71d2b4ecafa887172208e05
sirocco1 <shino2163@gmail.com>
2019-03-09 19:19:00 +0900

    Add: いいね機能に非同期通信機能を追加
    ・いいね機能に非同期通信で表示が変わるように追加
    ・リダイレクト先の記述を削除

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,13 +1,29 @@
 class FavoritesController < ApplicationController
+  # def create
+  #   @favorite = Favorite.create(user_id: current_user.id, picture_id: params[:picture_id])
+  #   @favorites = Favorite.where(picture_id: params[:picture_id])
+  #   @pictures = Picture.all
+  # end
+
+  # def destroy
+  #   favorite = Favorite.find_by(user_id: current_user.id, picture_id: params[:picture_id])
+  #   favorite.destroy
+  #   @favorites = Favorite.where(picture_id: params[:picture_id])
+  #   @pictures = Picture.all
+  # end
+
   def create
-  	favorite = current_user.favorites.build(picture_id: params[:picture_id])
+    @picture = Picture.find(params[:picture_id])
+    favorite = current_user.favorites.build(picture_id: params[:picture_id])
     favorite.save
-    redirect_to users_path
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-  	favorite = Favorite.find_by(picture_id: params[:picture_id], user_id: current_user.id)
+    @picture = Picture.find(params[:picture_id])
+    favorite = Favorite.find_by(picture_id: params[:picture_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to users_path
+    # redirect_back(fallback_location: root_path)
   end
+
 end
