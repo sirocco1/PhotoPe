0f47647e7b5175364d5e1da12735b99a5bd94a7e
sirocco1 <shino2163@gmail.com>
2019-03-18 17:29:53 +0900

    Add: ヘッダーレイアウト追加（途中）　

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :set_search
+
+	def set_search
+		@find = User.ransack(params[:q])
+  		@find_users = @find.result.page(params[:page])
+    end
 
 	protected
  	  def configure_permitted_parameters
